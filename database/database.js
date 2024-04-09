24f0a491b1c5283b55d93eaf2cb914f8d556d04d
Cristopher Teixeira <cristopher.teixeira04@gamil.com>
2024-04-09 15:07:08 -0300

    Alteração feita no database.js

--- database/database.js
+++ database/database.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 // Para conseguir utilizar as variaveis do .env
 dotenv.config();
 
+// Substitua as "${process.env}" pelo nome do seu banco de dados, nome do usuário do workbench e a senha do seu usuário no workbench
 const connection = new Sequelize(
   `${process.env.DB_NAME}`,
   `${process.env.HOST_NAME}`,
